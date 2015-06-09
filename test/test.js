0be076c3ac2a1ad24e112cf89149468aff37c061
Slobodan <slobodan@cloudhorizon.com>
2015-06-09 20:50:08 +0200

    feat: add support for full Russian alphabet
    Add support for full Russian alphabet, add test for Russian language and increase version to 2.0.0.

--- test/test.js
+++ test/test.js
@@ -2,7 +2,11 @@ var test = require('tape')
 var converter = require('../cyrillicToLatin')
 
 test('Cyrillic to latin conversion test', function (t) {
-  t.plan(1)
+  t.plan(2)
   
-  t.equal(converter('Ћирилица је писмо које користи седам словенских језика (белоруски, бугарски, македонски, руски, српски, украјински и де јуре бошњачки)'), 'Ćirilica je pismo koje koristi sedam slovenskih jezika (beloruski, bugarski, makedonski, ruski, srpski, ukrajinski i de jure bošnjački)')
+  t.equal(converter('Ћирилица је писмо које користи седам словенских језика (белоруски, бугарски, македонски, руски, српски, украјински и де јуре бошњачки)'),
+                    'Ćirilica je pismo koje koristi sedam slovenskih jezika (beloruski, bugarski, makedonski, ruski, srpski, ukrajinski i de jure bošnjački)')
+  
+  t.equal(converter('Около 863 года братья Кирилл и Мефодий из Солуни (Салоники) по приказу византийского императора Михаила III упорядочили письменность для славянского языка.'),
+                    'Okolo 863 goda bratʹâ Kirill i Mefodij iz Soluni (Saloniki) po prikazu vizantijskogo imperatora Mihaila III uporâdočili pisʹmennostʹ dlâ slavânskogo âzyka.')
 })
